b7a6fdb72850f88b5bcca526c667cde19879f75c
Carlo_Munizza <cam7134@psu.edu>
2024-05-20 21:55:57 -0400

    added colors and logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import rocket from './rocket.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App-background">
-      <nav>
+    <div className="App-background Primary">
+      <nav style={{color: 'var(--secondary-color)'}}>
         hello
       </nav>
       <div style={{height: '10vh'}}></div>
 
       <div className="App">
-          <h1>Galactic Mining Game</h1>
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="Secondary" style={{color: 'var(--secondary-color)'}}>Galactic Mining Game</h1>
+          <img src={rocket} className="App-logo" alt="rocket" />
           <dev style={{display: 'grid'}}>
-            <label>
+            <label style={{color: 'var(--secondary-color)'}}>
               Username: <input type="text" name="name"/>
             </label>
       
-            <label>
-              Password: <input type="text" name="name" />
+            <label style={{color: 'var(--secondary-color)'}}>
+              Password: <input type="text" name="name"/>
             </label>
           </dev>
       </div>
