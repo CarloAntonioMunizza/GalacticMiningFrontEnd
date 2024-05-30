5ae5b75819e7a18c46fbaa6c97c73beb7f712d82
Carlo_Munizza <cam7134@psu.edu>
2024-05-30 01:59:01 -0400

    finished homepage, added dropdown menu, created spinning animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import rocket from './rocket.svg';
 import './App.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function App() {
   return (
     <div className="App-background Primary">
-      <nav style={{color: 'var(--secondary-color)'}}>
-        hello
-      </nav>
-      <div style={{height: '10vh'}}></div>
-
       <div className="App">
-          <h1 className="Secondary" style={{color: 'var(--secondary-color)'}}>Galactic Mining Game</h1>
-          <img src={rocket} className="App-logo" alt="rocket" />
-          <dev style={{display: 'grid'}}>
-            <label style={{color: 'var(--secondary-color)'}}>
-              Username: <input type="text" name="name"/>
-            </label>
+        <Dropdown data-bs-theme='dark'>
+          <Dropdown.Toggle variant="success" id="dropdown-button">
+            Additional Info
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Power BI</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <h1 className="Secondary" style={{color: 'var(--secondary-color)'}}>Galactic Mining Game</h1>
+        <div className="App-logo" >
+          <img style={{height: '10vh', rotate: '45deg'}} src={rocket} alt="rocket" />
+        </div>
       
-            <label style={{color: 'var(--secondary-color)'}}>
-              Password: <input type="text" name="name"/>
-            </label>
-          </dev>
+        <div style={{height: '5vh'}}></div>
+
+        <div style={{display: 'grid', gridTemplateColumns: '1fr', gridGap: '10px'}}>
+          <label style={{color: 'var(--secondary-color)'}}>
+            Username: <input type="text" name="name"/>
+          </label>
+
+          <label style={{color: 'var(--secondary-color)'}}>
+            Password: <input type="text" name="name"/>
+          </label>
+        </div>
       </div>
     </div>
   );
